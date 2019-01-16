16dc63457179ed14f3b3aa54579ed54cdb4ed01f
Mikael Myyrä <mikael.myyrae@gmail.com>
2019-01-16 15:06:37 +0200

    Add derive macro to create ComponentFilters

--- engine/ecs/src/system.rs
+++ engine/ecs/src/system.rs
@@ -5,7 +5,7 @@ pub use moleengine_ecs_codegen::*;
 
 /// A System can perform arbitrary operations on game objects with desired associated Components,
 /// which are defined as a ComponentFilter.
-/// They are executed within a Space with Space::run_system::<Type>() or Space::run_optional_system::<Type>().
+/// They are executed within a Space with Space::(try_)run_system::<Type>().
 pub trait System<'a> {
     type Filter: ComponentFilter<'a>;
     fn operate(items: &mut [Self::Filter], space: &Space, queue: &mut EventQueue);
@@ -26,7 +26,7 @@ impl<'a, S: SimpleSystem<'a>> System<'a> for S {
 }
 
 /// A set of Components that knows how to extract itself from a Space.
-/// These do not need to be defined by hand - there is a derive macro available
+/// These do not need to be implemented by hand - there is a derive macro available
 /// in the moleengine_ecs_codegen crate.
 /// # Example
 /// ```
@@ -52,45 +52,10 @@ pub struct Velocity {
     pub y: f32,
 }
 
-//#[derive(ComponentFilter)]
+#[derive(ComponentFilter)]
 pub struct PosVel<'a> {
-    /*#[id]*/ id: IdType,
     position: &'a mut Position,
     velocity: &'a Velocity,
-}
-
-impl<'a> ComponentFilter<'a> for PosVel<'a> {
-    fn run(space: &Space, mut f: impl FnMut(&mut [Self])) -> Option<()> {
-        let position = space.try_open_container::<Position>()?;
-        let velocity = space.try_open_container::<Velocity>()?;
-        let mut position_access = position.write();
-        let velocity_access = velocity.read();
-
-        let alive = space.get_alive();
-        let position_users = position.get_users();
-        let velocity_users = velocity.get_users();
-
-        let and_set = hibitset::BitSetAll;
-        let and_set = hibitset::BitSetAnd(position_users, and_set);
-        let and_set = hibitset::BitSetAnd(velocity_users, and_set);
-        let and_set = hibitset::BitSetAnd(alive, and_set);
-
-        use hibitset::BitSetLike;
-        let iter = and_set.iter();
-        let mut items: Vec<_> = iter
-            .map(|id| unsafe {
-                PosVel {
-                    position: position_access.get_mut_raw(id as IdType).as_mut().unwrap(),
-                    velocity: velocity_access.get_raw(id as IdType).as_ref().unwrap(),
-                    id: id as IdType,
-                }
-            })
-            .collect();
-
-        f(items.as_mut_slice());
-
-        Some(())
-    }
 }
 
 pub struct Mover;
--- engine/ecs_codegen/src/lib.rs
+++ engine/ecs_codegen/src/lib.rs
@@ -10,28 +10,28 @@ use syn;
 use proc_macro::TokenStream;
 use quote::quote;
 
-// the macros here translate to something like the following test
-// (unless I've changed them and forgotten to update this, which is not unlikely):
+// this macro translates to something like the following test
+// (unless I've changed it and forgotten to update this, which is not unlikely):
 //
-// #[derive(Debug)]
-// pub struct Position {
-//     pub x: f32,
-//     pub y: f32,
-// }
-// pub struct Velocity {
-//     pub x: f32,
-//     pub y: f32,
-// }
+//#[derive(Debug)]
+//pub struct Position {
+//    pub x: f32,
+//    pub y: f32,
+//}
+//pub struct Velocity {
+//    pub x: f32,
+//    pub y: f32,
+//}
 //
-// //#[system_item]
-// pub struct Mover<'a> {
-//     position: &'a mut Position,
-//     velocity: &'a Velocity,
-// }
+//#[derive(ComponentFilter)]
+//pub struct PosVel<'a> {
+//    #[id] id: IdType,
+//    position: &'a mut Position,
+//    velocity: &'a Velocity,
+//}
 //
-//pub struct MoverRunner;
-//impl SystemRunner for MoverRunner {
-//    fn run(space: &Space) -> Option<()> {
+//impl<'a> ComponentFilter<'a> for PosVel<'a> {
+//    fn run(space: &Space, mut f: impl FnMut(&mut [Self])) -> Option<()> {
 //        let position = space.try_open_container::<Position>()?;
 //        let velocity = space.try_open_container::<Velocity>()?;
 //        let mut position_access = position.write();
@@ -46,115 +46,121 @@ use quote::quote;
 //        let and_set = hibitset::BitSetAnd(velocity_users, and_set);
 //        let and_set = hibitset::BitSetAnd(alive, and_set);
 //
+//        use hibitset::BitSetLike;
 //        let iter = and_set.iter();
 //        let mut items: Vec<_> = iter
 //            .map(|id| unsafe {
-//                Mover {
+//                PosVel {
 //                    position: position_access.get_mut_raw(id as IdType).as_mut().unwrap(),
-//                    velocity: velocity_access.get(id as IdType),
+//                    velocity: velocity_access.get_raw(id as IdType).as_ref().unwrap(),
+//                    id: id as IdType,
 //                }
 //            })
 //            .collect();
 //
-//        Mover::operate(items.as_mut_slice());
+//        f(items.as_mut_slice());
 //
 //        Some(())
 //    }
 //}
-//
-//impl<'a> System for Mover<'a> {
-//    type Runner = MoverRunner;
-//    //#[system_logic]
-//    fn operate(items: &mut [Self]) {
-//        for item in items {
-//             item.position.x += item.velocity.x;
-//             item.position.y += item.velocity.y;
-//        }
-//    }
-//}
 
-/// This attribute denotes a struct that gathers Components for a System to use.
-/// Declaring a field as mutable gives write access to the corresponding Component.
+/// This attribute denotes a struct that gathers Components from a Space, primarily used by Systems
+/// but available elsewhere as well.
+/// Declaring a field as mutable gives write access to the corresponding component.
 ///
-/// Fields must be named and they must all be reference types.
-/// This must be used in conjunction with a function with the #[system_logic] attribute
-/// to operate on the data this attribute provides; otherwise you will get a compiler error.
-///
-/// See system_logic for usage information.
-#[proc_macro_attribute]
-pub fn system_item(_attr: TokenStream, item: TokenStream) -> TokenStream {
+/// The struct must have exactly one lifetime parameter.
+/// Fields must be named and they must all be reference types. Additionally, an optional IdType field
+/// may be provided with the #[id] attribute if you need to identify objects.
+/// #Example
+/// ```
+///#[derive(ComponentFilter)]
+///pub struct PosVel<'a> {
+///    #[id] id: IdType,
+///    position: &'a mut Position,
+///    velocity: &'a Velocity,
+///}
+///```
+#[proc_macro_derive(ComponentFilter, attributes(id))]
+pub fn system_item(item: TokenStream) -> TokenStream {
     let input = syn::parse_macro_input!(item as syn::ItemStruct);
-    let vis = &input.vis;
     let ident = &input.ident;
     let fields = &input.fields;
-
-    let runner_ident = append_ident(ident, "Runner");
 
     let mut accesses = Vec::new();
     let mut users_idents = Vec::new();
     let mut field_getters = Vec::new();
+    let mut id_field = None;
     for field in fields {
-        let ident = field.ident.as_ref().expect("Struct fields must be named");
-        let access_ident = append_ident(ident, "_access");
-        let users_ident = append_ident(ident, "_users");
+        if is_id(field) {
+            id_field = field.ident.as_ref();
+        } else {
+            let ident = field.ident.as_ref().expect("Struct fields must be named");
+            let access_ident = append_ident(ident, "_access");
+            let users_ident = append_ident(ident, "_users");
 
-        let field_type_ref = match &field.ty {
-            syn::Type::Reference(r) => r,
-            _ => panic!("System must only contain reference types"),
-        };
-        let ty = &field_type_ref.elem;
+            let field_type_ref = match &field.ty {
+                syn::Type::Reference(r) => r,
+                _ => panic!("Filter must only contain reference types (maybe you're missing a #[id] attribute?)"),
+            };
+            let ty = &field_type_ref.elem;
 
-        match field_type_ref.mutability {
-            Some(_) => {
-                let access = quote! {
-                    let #ident = space.try_open_container::<#ty>()?;
-                    let mut #access_ident = #ident.write();
-                    let #users_ident = #ident.get_users();
-                };
-                accesses.push(access);
+            match field_type_ref.mutability {
+                Some(_) => {
+                    let access = quote! {
+                        let #ident = space.try_open_container::<#ty>()?;
+                        let mut #access_ident = #ident.write();
+                        let #users_ident = #ident.get_users();
+                    };
+                    accesses.push(access);
 
-                let getter = quote! {
-                    #ident: #access_ident.get_mut_raw(id as IdType).as_mut().unwrap(),
-                };
-                field_getters.push(getter);
+                    let getter = quote! {
+                        #ident: #access_ident.get_mut_raw(id as IdType).as_mut().unwrap(),
+                    };
+                    field_getters.push(getter);
+                }
+                None => {
+                    let access = quote! {
+                        let #ident = space.try_open_container::<#ty>()?;
+                        let #access_ident = #ident.read();
+                        let #users_ident = #ident.get_users();
+                    };
+                    accesses.push(access);
+
+                    let getter = quote! {
+                        #ident: #access_ident.get_raw(id as IdType).as_ref().unwrap(),
+                    };
+                    field_getters.push(getter);
+                }
             }
-            None => {
-                let access = quote! {
-                    let #ident = space.try_open_container::<#ty>()?;
-                    let #access_ident = #ident.read();
-                    let #users_ident = #ident.get_users();
-                };
-                accesses.push(access);
-
-                let getter = quote! {
-                    #ident: #access_ident.get(id as IdType),
-                };
-                field_getters.push(getter);
-            }
+            users_idents.push(users_ident);
         }
-        users_idents.push(users_ident);
     }
 
-    let runner = quote! {
-        #input
+    let id_setter = id_field.map(|ident| {
+        quote! {
+            #ident: id as IdType,
+        }
+    });
 
-        #vis struct #runner_ident;
-        impl SystemRunner for #runner_ident {
-            fn run(space: &Space) -> Option<()> {
+    let runner = quote! {
+        impl<'a> ComponentFilter<'a> for #ident<'a> {
+            fn run(space: &Space, mut f: impl FnMut(&mut [Self])) -> Option<()> {
                 #(#accesses)*
 
                 let alive = space.get_alive();
                 let and_set = hibitset::BitSetAll{};
                 #(let and_set = hibitset::BitSetAnd(and_set, #users_idents);)*
-                let iter = and_set.iter();
 
+                use hibitset::BitSetLike;
+                let iter = and_set.iter();
                 let mut items: Vec<_> = iter.map(|id| unsafe {
                     #ident {
                         #(#field_getters)*
+                        #id_setter
                     }
                 }).collect();
 
-                #ident::operate(items.as_mut_slice());
+                f(items.as_mut_slice());
 
                 Some(())
             }
@@ -164,84 +170,16 @@ pub fn system_item(_attr: TokenStream, item: TokenStream) -> TokenStream {
     runner.into()
 }
 
-const MSG_ARG_MISMATCH: &str =
-    "System logic function must take a mutable slice of a system data struct as argument";
+fn is_id(field: &syn::Field) -> bool {
+    for attr in &field.attrs {
+        let meta = attr.parse_meta().unwrap();
+        match meta {
+            syn::Meta::Word(ref ident) if ident == "id" => return true,
+            _ => (),
+        };
+    }
 
-/// This attribute denotes a function that operates on a struct marked with #[system_item]
-///
-/// The function must take as its only argument a mutable slice of a system_item.
-///
-/// # Example
-/// ```
-/// #[system_item]
-/// pub struct Mover<'a> {
-///     position: &'a mut Position,
-///     velocity: &'a Velocity,
-/// }
-///
-/// #[system_logic]
-/// fn do_move(items: &mut [Mover<'_>]) {
-///     for item in items {
-///         item.position.x += item.velocity.x;
-///         item.position.y += item.velocity.y;
-///     }
-/// }
-/// ```
-/// Running this with `Space::run_system::<Mover>()` gives do_move the position (read/write)
-/// and the velocity (read only) of every Object in the Space that has both of them associated with it.
-#[proc_macro_attribute]
-pub fn system_logic(_attr: TokenStream, item: TokenStream) -> TokenStream {
-    let input = syn::parse_macro_input! {item as syn::ItemFn};
-    let block = &input.block;
-    let arg = input
-        .decl
-        .inputs
-        .first()
-        .expect("System logic function must have one argument")
-        .into_value();
-    // the argument is a Captured Reference to a Slice of the desired type.
-    // this gets a little cumbersome to write
-    let arg_cap = match arg {
-        syn::FnArg::Captured(a) => a,
-        _ => panic!(MSG_ARG_MISMATCH),
-    };
-    let arg_ref = match &arg_cap.ty {
-        syn::Type::Reference(r) => r,
-        _ => panic!(MSG_ARG_MISMATCH),
-    };
-    arg_ref.mutability.expect(MSG_ARG_MISMATCH);
-    let arg_slice = match &*arg_ref.elem {
-        syn::Type::Slice(s) => s,
-        _ => panic!(MSG_ARG_MISMATCH),
-    };
-    let type_ident = match &*arg_slice.elem {
-        syn::Type::Path(p) => {
-            &p.path
-                .segments
-                .last()
-                .expect(MSG_ARG_MISMATCH)
-                .into_value()
-                .ident
-        }
-        _ => panic!(MSG_ARG_MISMATCH),
-    };
-    let runner_ident = append_ident(type_ident, "Runner");
-
-    let arg_ident = match &arg_cap.pat {
-        syn::Pat::Ident(i) => &i.ident,
-        _ => panic!("Invalid function argument identifier"),
-    };
-
-    let result = quote! {
-        impl System for #type_ident<'_> {
-            type Runner = #runner_ident;
-            fn operate (#arg_ident: &mut [Self]) {
-                #block
-            }
-        }
-    };
-
-    result.into()
+    false
 }
 
 fn append_ident(ident: &syn::Ident, postfix: &str) -> syn::Ident {
