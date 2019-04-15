59bf64f9f94f017c428b7d713c3e0a5afdda9a62
Mikael Myyrä <mikael.myyrae@gmail.com>
2019-04-15 22:28:46 +0300

    WIP: use custom loop and glium in template

--- examples/template/src/main.rs
+++ examples/template/src/main.rs
@@ -1,6 +1,71 @@
-mod gameloop;
+mod states;
+
+use glium::glutin;
+
+use moleengine::{
+    ecs::{
+        recipe::{ObjectRecipe, RecipeBook},
+        space::Space,
+        storage::VecStorage,
+    },
+    physics2d::RigidBody,
+    util::{inputstate::*, Transform},
+    visuals::shape::*,
+};
+
+//
+
+pub struct Resources {
+    pub display: glium::Display,
+    pub events: glutin::EventsLoop,
+    pub input_state: InputState,
+    pub recipes: RecipeBook,
+    pub space: Space,
+}
 
 fn main() {
-    let res = gameloop::init();
-    gameloop::begin(res);
+    let res = init_resources();
+    states::begin(res);
+}
+
+pub fn init_resources() -> Resources {
+    let mut events = glutin::EventsLoop::new();
+    let window = glutin::WindowBuilder::new();
+    let context = glutin::ContextBuilder::new();
+    let display = glium::Display::new(window, context, &events).expect("Failed to create display");
+
+    let mut input_state = InputState::new();
+    {
+        //use Key::*;
+        //input_state.track_keys(&[Up, Down, Left, Right]);
+    }
+
+    let mut space = Space::with_capacity(100);
+    space
+        .add_container::<Transform, VecStorage<_>>()
+        .add_container::<RigidBody, VecStorage<_>>()
+        .add_container::<Shape, VecStorage<_>>();
+
+    let mut recipes = RecipeBook::new();
+    let mut block = ObjectRecipe::new()
+        .add_variable(Some(Transform::from_position([150.0, 150.0])))
+        .add({
+            let mut rb = RigidBody::new();
+            rb.angular_vel = 0.03;
+            rb
+        })
+        .add(Shape::new_square(80.0, [1.0; 4]));
+    block.apply(&mut space);
+    block.set_variable(Transform::from_position([300.0, 300.0]));
+    block.apply(&mut space);
+    block.reset_variables();
+    recipes.add("block", block);
+
+    Resources {
+        display,
+        events,
+        input_state,
+        recipes,
+        space,
+    }
 }
---
+++ examples/template/src/states.rs
@@ -0,0 +1,103 @@
+use glium::{glutin, Surface};
+
+use moleengine::{
+    ecs::space::Space,
+    physics2d::systems::Motion,
+    util::{
+        gameloop::{GameLoop, LockstepLoop},
+        statemachine::{GameState, StateMachine, StateOp},
+    },
+    visuals::shape::*,
+};
+
+use crate::Resources;
+
+const BG_COLOR: [f32; 4] = [0.1, 0.1, 0.1, 1.0];
+const LINE_COLOR: [f32; 4] = [0.729, 0.855, 0.333, 1.0];
+
+// ============= Begin ===============
+
+pub fn begin(res: Resources) {
+    let mut sm = StateMachine::new(res, Box::new(StatePlaying));
+    let l = LockstepLoop::from_fps(60);
+    l.begin(&mut sm);
+}
+
+// ============= Playing ===============
+
+pub struct StatePlaying;
+
+impl GameState<Resources> for StatePlaying {
+    fn update(&mut self, res: &mut Resources) -> StateOp<Resources> {
+        // TODO these are ugly as hell, come up with a better alternative
+        let mut should_close = false;
+        let mut should_pause = false;
+        res.events.poll_events(|evt| match evt {
+            glutin::Event::WindowEvent { event, .. } => match event {
+                glutin::WindowEvent::CloseRequested => should_close = true,
+                glutin::WindowEvent::KeyboardInput { input, .. } => match input.virtual_keycode {
+                    Some(glutin::VirtualKeyCode::Space) => should_pause = true,
+                    Some(glutin::VirtualKeyCode::Escape) => should_close = true,
+                    _ => (),
+                },
+                _ => (),
+            },
+            _ => (),
+        });
+        if should_close {
+            return StateOp::Destroy;
+        }
+        if should_pause {
+            return StateOp::Push(Box::new(StatePaused));
+        }
+
+        update_space(&mut res.space);
+        res.input_state.update_ages();
+
+        println!("updated");
+
+        StateOp::Stay
+    }
+
+    fn render(&mut self, res: &mut Resources) {
+        let mut target = res.display.draw();
+        target.clear_color(BG_COLOR[0], BG_COLOR[1], BG_COLOR[2], BG_COLOR[3]);
+        target.finish().unwrap();
+
+        println!("rendered");
+    }
+}
+
+// ============= Paused ===============
+
+pub struct StatePaused;
+
+impl GameState<Resources> for StatePaused {
+    fn update(&mut self, res: &mut Resources) -> StateOp<Resources> {
+        //while let Some(evt) = res.window.next() {
+        //    if let Some(Button::Keyboard(Key::Space)) = evt.press_args() {
+        //        return StateOp::Pop;
+        //    }
+        //}
+
+        StateOp::Stay
+    }
+
+    fn render(&mut self, res: &mut Resources) {}
+}
+
+// ============== Helper functions ==================
+
+//fn draw_space(gl: &mut GlGraphics, args: RenderArgs, space: &mut Space) {
+//    let ctx = gl.draw_begin(args.viewport());
+//
+//    graphics::clear(BG_COLOR, gl);
+//
+//    space.run_system(ShapeRenderer::new(&ctx, gl));
+//
+//    gl.draw_end();
+//}
+
+fn update_space(space: &mut Space) {
+    space.run_system(Motion);
+}
