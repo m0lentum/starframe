5a8b258e72a1b885aa0850d6f33c7ee71051511b
Mikael Myyrä <mikael.myyrae@gmail.com>
2020-06-17 21:46:49 +0300

    Move all nix expressions to shell.nix, remove niv
    Didn't feel right to have this much stuff
    just to get rust and a bit of stuff from nixpkgs.

--- nix/rust.nix
+++
@@ -1,9 +0,0 @@
-{ sources ? import ./sources.nix }:
-
-let
-  pkgs =
-    import sources.nixpkgs { overlays = [ (import sources.nixpkgs-mozilla) ]; };
-in
-pkgs.latest.rustChannels.stable.rust.override {
-  extensions = [ "rust-src" ];
-}
--- shell.nix
+++ shell.nix
@@ -1,15 +1,26 @@
 let
-  sources = import ./nix/sources.nix;
-  rust = import ./nix/rust.nix { inherit sources; };
-  pkgs = import sources.nixpkgs {};
+  moz-overlay = import (fetchTarball {
+    url = "https://github.com/mozilla/nixpkgs-mozilla/archive/e912ed483e980dfb4666ae0ed17845c4220e5e7c.tar.gz";
+    sha256 = "08fvzb8w80bkkabc1iyhzd15f4sm7ra10jn32kfch5klgl0gj3j3";
+  });
+
+  pkgs = import (fetchTarball {
+    # nixos-20.03
+    url = "https://github.com/NixOS/nixpkgs-channels/archive/ab3adfe1c769c22b6629e59ea0ef88ec8ee4563f.tar.gz";
+    sha256 = "1m4wvrrcvif198ssqbdw897c8h84l0cy7q75lyfzdsz9khm1y2n1";
+  }) { overlays = [ moz-overlay ]; };
+
+  rust = pkgs.latest.rustChannels.stable.rust.override {
+    extensions = [ "rust-src" ];
+  };
 in
 pkgs.mkShell {
-  buildInputs = [
+  buildInputs = with pkgs; [
     rust
     # for wgpu
-    pkgs.pkgconfig
-    pkgs.xlibs.libX11
-    pkgs.shaderc
+    pkgconfig
+    xlibs.libX11
+    shaderc
   ];
   # make graphics libraries available
   LD_LIBRARY_PATH = with pkgs.xlibs; "${libX11}/lib:${libXcursor}/lib:${libXxf86vm}/lib:${libXi}/lib:${libXrandr}/lib:${pkgs.vulkan-loader}/lib";
